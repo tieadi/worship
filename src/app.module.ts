0a2eb5937824884f3e33ec706c1c18468fc532c7
tiaditatiaia <ti.aditatiaia@gmail.com>
2022-07-25 16:15:23 -0300

    reconfig app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { VisitorsModule } from './visitors/visitors.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ envFilePath: '.env' }),
-    MongooseModule.forRoot(process.env.DATABASE_HOST),
-    VisitorsModule,
-  ],
+  imports: [MongooseModule.forRoot(process.env.DATABASE_HOST), VisitorsModule],
   controllers: [],
   providers: [],
 })
